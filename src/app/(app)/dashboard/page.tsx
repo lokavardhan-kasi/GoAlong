892be4d4d557628121251d88a45b7cefc0b77d26
Lokavardhan Kasi <kasilokavardhan200@gmail.com>
2026-01-08 11:52:04 +0000

    when we click on the tick mark on the your published raid routes it shou

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { motion } from 'framer-motion';
@@ -325,15 +324,5 @@ export default function DashboardPage() {
     </div>
   );
 }
-
-    
-
-    
-
-
-
-    
-
-    
 
     
